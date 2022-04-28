e915d3603177b1c6e324b7f9f03b6bef1aa19de9
MaxKrasnoloskyi <makskrasnopolskyi@gmail.com>
2022-04-29 00:54:39 +0300

    Initial commit

---
+++ src/test/java/Task1Test.java
@@ -0,0 +1,24 @@
+import org.junit.Before;
+import org.junit.BeforeClass;
+import org.junit.Test;
+
+import static org.junit.Assert.*;
+
+public class Task1Test {
+
+    @Test
+    public void numJewelsInStones1() {
+        Task1 object = new Task1();
+        assertEquals(3,object.numJewelsInStones("aA", "aAAbbbb"));
+    }
+    @Test
+    public void numJewelsInStones2(){
+        Task1 object = new Task1();
+        assertEquals(0,object.numJewelsInStones("x", "XX"));
+    }
+    @Test
+    public void numJewelsInStones3(){
+        Task1 object = new Task1();
+        assertNotEquals(2,object.numJewelsInStones("k", "kyi"));
+    }
+}
