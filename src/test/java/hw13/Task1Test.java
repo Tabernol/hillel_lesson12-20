121eaea93c5f985b1f2101fc8fcb6e2fd7cdc462
MaxKrasnoloskyi <makskrasnopolskyi@gmail.com>
2022-05-04 01:09:46 +0300

    hw13

---
+++ src/test/java/hw13/Task1Test.java
@@ -0,0 +1,20 @@
+package hw13;
+
+import org.junit.Before;
+import org.junit.Test;
+
+import static org.junit.Assert.*;
+
+public class Task1Test {
+    Task1 task = new Task1();
+
+    @Test
+    public void maxProduct1() {
+        assertEquals(30,task.maxProduct(new int[]{2,4,6,1,7}));
+    }
+
+    @Test
+    public void maxProduct2(){
+        assertEquals(0,task.maxProduct(new int[]{1,1}));
+    }
+}
