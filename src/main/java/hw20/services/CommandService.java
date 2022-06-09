bee8e1f5e2e611e7ae38207fba34c95f7d1d7e55
MaxKrasnoloskyi <makskrasnopolskyi@gmail.com>
2022-06-10 00:09:19 +0300

    hw20

--- src/main/java/hw20/services/CommandService.java
+++ src/main/java/hw20/services/CommandService.java
@@ -1,91 +1,62 @@
 package hw20.services;
 
-import hw17.model.Book;
-import hw17.model.Output;
+import hw20.model.*;
 
-import java.io.BufferedWriter;
-import java.io.FileWriter;
-import java.io.IOException;
+import java.sql.*;
 
 public class CommandService extends ServiceLibrary {
-    Book bookDefault = new Book("unknown", "unknown", "unknown");
 
     public Book createBook() {
         var bookBuilder = Book.builder();
-        System.out.println(Output.NAME);
+        System.out.println(Output.NAME.description);
         bookBuilder.nameOfBook(String.valueOf(scanner.nextLine()));
-        System.out.println(Output.AUTHOR);
-        bookBuilder.author(String.valueOf(scanner.nextLine())).build();
-        System.out.println(Output.TEXT);
-        String line = "";
-        StringBuilder allText = new StringBuilder();
-        while (!check(line = scanner.nextLine())) {
-            allText.append(line + "\n");
-        }
-        Book book = bookBuilder.text(allText.toString()).build();
+        System.out.println(Output.AUTHOR.description);
+        bookBuilder.author(String.valueOf(scanner.nextLine()));
+        Book book = bookBuilder.build();
         return book;
     }
 
-    public String add() {
+    public void add() {
         Book book = createBook();
-        library.getCatalog().put(book.getNameOfBook(), book);
-        saveLibrary();
-        System.out.println(Output.ADD);
-        return Output.ADD;
-    }
-
-    public void read() {
-        System.out.println(Output.NAME);
-        String choice = scanner.nextLine();
-        System.out.println(library.getCatalog().getOrDefault(choice, bookDefault).getText());
+        try {
+            Statement stmt = connect.createStatement();
+            stmt.executeUpdate("INSERT INTO my_library(author, name_of_book) VALUES ('"
+                    + book.getAuthor() + "' , '" + book.getNameOfBook() + "')");
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
+        System.out.println(Output.ADD.description);
     }
 
     public void delete() {
-        System.out.println(Output.NAME);
-        String line = scanner.nextLine();
-        if (library.getCatalog().containsKey(line)) {
-            library.getCatalog().remove(line);
-            saveLibrary();
-            System.out.println(Output.DELETE);
-        } else {
-            System.out.println(bookDefault.getText());
-        }
-    }
-
-    public String exit() {
-        return "exit";
-    }
-
-    public void show() {
-        library.getCatalog().forEach((k, v) -> System.out.println(k));
-    }
-
-    public void saveLibrary() {
+        System.out.println(Output.ID.description);
+        int row = scanner.nextInt();
         try {
-            jsonMapper.writeValue(directory, library);
-        } catch (IOException e) {
+            Statement stmt = connect.createStatement();
+            stmt.executeUpdate("DELETE FROM my_library WHERE id = " + String.valueOf(row));
+        } catch (SQLException e) {
             e.printStackTrace();
         }
     }
 
-    public void saveToFile() {
-        System.out.println(Output.NAME);
-        String choice = scanner.nextLine();
-        if (library.getCatalog().containsKey(choice)) {
-            String allText = library.getCatalog().get(choice).getText();
-            try (BufferedWriter bw = new BufferedWriter(
-                    new FileWriter(Output.PATH + choice + ".txt"))) {
-                bw.write(allText);
-                System.out.println(Output.SAVED + Output.PATH + choice + ".txt");
-            } catch (IOException e) {
-                e.printStackTrace();
-            }
-        } else {
-            System.out.println(bookDefault.getText());
+    public void exit() {
+        try {
+            connect.close();
+        } catch (SQLException e) {
+            e.printStackTrace();
         }
-
-
     }
 
-
+    public void show() {
+        try {
+            Statement stmt = connect.createStatement();
+            ResultSet rs = stmt.executeQuery("select * from my_library");
+            while (rs.next()) {
+                System.out.println(rs.getInt(1) + "  "
+                        + rs.getString(2) + " " + rs.getString(3));
+            }
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
+    }
 }
--- src/main/java/hw20/services/ServiceLibrary.java
+++ src/main/java/hw20/services/ServiceLibrary.java
@@ -1,31 +1,31 @@
 package hw20.services;
 
-import com.fasterxml.jackson.databind.json.JsonMapper;
-import hw17.model.Command;
-import hw17.model.Library;
-import hw17.model.Output;
+import hw20.model.*;
 
-import java.io.File;
-import java.io.IOException;
-import java.util.HashMap;
-import java.util.Map;
-import java.util.Scanner;
+import java.sql.*;
+import java.util.*;
 import java.util.function.Consumer;
 
-public class ServiceLibrary implements CheckAble {
-    static File directory = new File(Output.PATH + "lib.json");
-    static Library library = new Library();
+public class ServiceLibrary implements CheckAble, ConnectDAO {
     private static Map<String, Consumer<Scanner>> mapCommand = new HashMap<>();
     private static CommandService commandService = new CommandService();
-    static JsonMapper jsonMapper = new JsonMapper();
+    private static final String URL = "jdbc:oracle:thin:@localhost:1521:xe";
+    private static final String user = "hr";
+    private static final String password = "hr";
+    static Connection connect;
     Scanner scanner = new Scanner(System.in);
 
     static {
         commandInitializer();
-        makeOrReadCatalog();
     }
 
     public void startLibrary() {
+        myConnection();
+        if (!connectionStatus()) {
+            System.out.println(Output.CONNECT_SUCCESS.description);
+        } else {
+            System.out.println(Output.NO_CONNECT.description);
+        }
         String command = "";
         while (!check(command)) {
             Command.printDescription();
@@ -37,19 +37,19 @@ public class ServiceLibrary implements CheckAble {
     private static void commandInitializer() {
         mapCommand.put(Command.SHOW.command, scanner -> commandService.show());
         mapCommand.put(Command.ADD.command, scanner -> commandService.add());
-        mapCommand.put(Command.READ.command, scanner -> commandService.read());
         mapCommand.put(Command.DELETE.command, scanner -> commandService.delete());
         mapCommand.put(Command.EXIT.command, scanner -> commandService.exit());
-        mapCommand.put(Command.SAVE.command, scanner -> commandService.saveToFile());
     }
 
-    public static void makeOrReadCatalog() {
+    public static boolean connectionStatus() {
+        boolean status = false;
         try {
-            directory.createNewFile();
-            library = jsonMapper.readValue(directory, Library.class);
-        } catch (IOException e) {
-            library = new Library();
+            status = connect.isClosed();
+        } catch (SQLException e) {
+            e.printStackTrace();
         }
+        return status;
+
     }
 
     @Override
@@ -57,10 +57,21 @@ public class ServiceLibrary implements CheckAble {
         return line.equals("exit");
     }
 
+
+    @Override
+    public Connection myConnection() {
+        try {
+            Class.forName("oracle.jdbc.driver.OracleDriver");
+            connect = DriverManager.getConnection(
+                    URL, user, password);
+        } catch (Exception e) {
+            System.out.println(e);
+        }
+        return connect;
+    }
+
     public static void main(String[] args) {
         ServiceLibrary start = new ServiceLibrary();
         start.startLibrary();
     }
-
-
 }
